8f5485d4fc2dcf1d94cc41bf574748fe5fa1db49
Zack Robinson <zwrprofessional@gmail.com>
2017-10-07 18:43:40 -0400

    Initial commit

---
+++ app/src/main/java/com/zackeryrobinson/githubandrestcalls/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zackeryrobinson.githubandrestcalls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // This is only here to meet the "submission" requirement.
+        // My next step would be to look through the submissions from the other students to see how they
+        // approached this.
+
+        // Retrofit and REST calls don't make sense to me.
+
+
+    }
+}
